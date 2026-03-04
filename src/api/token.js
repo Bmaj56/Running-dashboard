7dc189f495aad353a5b417735bc16c3a966ebf63
Bmaj56 <majmudarb@gmail.com>
2026-03-04 17:47:13 -0500

    Update token.js

--- src/api/token.js
+++ src/api/token.js
@@ -1,1 +1,15 @@
-api/token.js
+export default async function handler(req, res) {
+  const { code } = req.query;
+  const response = await fetch('https://www.strava.com/oauth/token', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      client_id: process.env.STRAVA_CLIENT_ID,
+      client_secret: process.env.STRAVA_CLIENT_SECRET,
+      code,
+      grant_type: 'authorization_code',
+    }),
+  });
+  const data = await response.json();
+  res.redirect(`/?token=${data.access_token}&athlete=${data.athlete.firstname}`);
+}
